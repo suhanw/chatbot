ec3c4f1330feeaa03c8e90e19d08f3ed16946607
Suhan Wijaya <suhanw@gmail.com>
2024-12-03 15:03:36 +0800

    Add Mongodb container

--- data/src/index.ts
+++ data/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = function () {
   return mongoose
-    .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/chatbot")
+    .connect(process.env.MONGODB_URI || "mongodb://data:27017/chatbotdb")
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
 };
