ab0b7a815898c25e93e414db3cba3f8d430c01de
Suhan Wijaya <suhanw@gmail.com>
2024-12-05 19:03:26 +0800

    Clean up

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -1,21 +1,22 @@
 import { fetchWrapper } from "../helpers";
 
 const GET_CURRENT_USER_PATH = "/api/auth/current_user";
-const LOGIN_PATH = "/api/auth/login";
-const SIGNUP_PATH = "/api/auth/signup";
+const POST_LOGIN_PATH = "/api/auth/login";
+const POST_SIGNUP_PATH = "/api/auth/signup";
+
 export const getCurrentUserApi = async () => {
   return await fetchWrapper(GET_CURRENT_USER_PATH);
 };
 
 export const loginApi = async (email: string, password: string) => {
-  return await fetchWrapper(LOGIN_PATH, {
+  return await fetchWrapper(POST_LOGIN_PATH, {
     method: "POST",
     body: JSON.stringify({ email, password }),
   });
 };
 
 export const signupApi = async (email: string, password: string) => {
-  return await fetchWrapper(SIGNUP_PATH, {
+  return await fetchWrapper(POST_SIGNUP_PATH, {
     method: "POST",
     body: JSON.stringify({ email, password }),
   });
